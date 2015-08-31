a7fb0a2bd4e881ae9c06c140d6b78e73b075caeb
kingera <kingera1818@gmail.com>
2015-08-31 17:08:20 -0400

    Assignment1
    Assigns each letter of the four letter word "zouk" to its ascii number
    equivalent. It then adds the numbers together and assigns the sum to the
    variable "wordTotal".

---
+++ assignment1/Assignment1.java
@@ -0,0 +1,16 @@
+package assignment1;
+
+public class Assignment1 {
+
+    public static void main(String[] args) {
+        String word = "zouk";
+        int letterOne = (int)'z';
+        int letterTwo = (int)'o';
+        int letterThree = (int)'u';
+        int letterFour = (int)'k';
+        int wordTotal = letterOne + letterTwo + letterThree + letterFour;
+        System.out.println(letterOne + " + " +letterTwo + " + " +letterThree + " + " + letterFour +
+                " = " + wordTotal);
+    }
+    
+}
